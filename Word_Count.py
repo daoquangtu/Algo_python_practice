cf0bec35703b12992cf4390b8e8535e7142444d6
Dao Quang Tu <85826073+daoquangtu@users.noreply.github.com>
2021-10-07 21:50:08 +0700

    Add files via upload

---
+++ Word_Count.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+#Input text:
+text = input("Nhập văn bản của bạn (50 words maximum):")
+
+#To count the word in text, we will count space appeard in the text. 
+count = 0
+for space in text:
+    if space == ' ':
+        count = count + 1
+        
+#Add 1 for the last word
+count = count + 1
+
+if count > 50: 
+    print("Your text has more than 50 words")
+else:
+    print(count)
+
+
+# In[ ]:
+
+
+
+
