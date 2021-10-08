5f953bfecd01a56248f9b71a1bfbc116576edcd4
Dao Quang Tu <85826073+daoquangtu@users.noreply.github.com>
2021-10-08 07:44:10 +0700

    Add files via upload

---
+++ count_vowel.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+text = input ("Nhập văn bản của bạn:")
+vowel_list = ["a","e","i","o","u","A","E","I","O","U"]
+count = 0
+for vowel in text:
+    if vowel in vowel_list:
+        count = count + 1
+print(count)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
