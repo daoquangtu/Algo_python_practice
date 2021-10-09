4456ac8c6470b1a5bed9278f4b60dd03f35ae8cc
Dao Quang Tu <85826073+daoquangtu@users.noreply.github.com>
2021-10-09 16:23:01 +0700

    Add files via upload

---
+++ Power_number.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Classical solution
+base_number = float(input('Your base number is:'))
+power_number = float(input('Your power number is:'))
+result = base_number ** power_number
+print("Your result is:",result)
+
+
+# In[2]:
+
+
+#Alternative solution
+base_number = float(input('Your base number is:'))
+power_number = float(input('Your power number is:'))
+result = pow(base_number, power_number)
+print("Your result is:",result)
+
+
+# In[ ]:
+
+
+
+
