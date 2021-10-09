53be411f4fc76f48583d21457756660c0661a7c5
Dao Quang Tu <85826073+daoquangtu@users.noreply.github.com>
2021-10-09 16:13:17 +0700

    Add files via upload

---
+++ convert_number.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def multiple(a,b):
+    a = int(a)
+    b= float(b)
+    multiple_result = a*b
+    return multiple_result
+
+
+# In[2]:
+
+
+print(multiple(3,2))
+
+
+# In[ ]:
+
+
+
+
