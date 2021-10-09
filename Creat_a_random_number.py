1aac29fdf1b3d242c39cffce1245c43dca60dc16
Dao Quang Tu <85826073+daoquangtu@users.noreply.github.com>
2021-10-09 16:48:15 +0700

    Add files via upload

---
+++ Creat_a_random_number.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+#We will creat a random number between 1 and 10, by import random library
+import random
+random_num = random.randint(0,10)
+print(random_num)
+
+
+# In[ ]:
+
+
+
+
