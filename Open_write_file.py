c60210e9adbd0e78b4a75033cf22fae3a1da42c2
Dao Quang Tu <85826073+daoquangtu@users.noreply.github.com>
2021-10-30 23:49:42 +0700

    Add files via upload

---
+++ Open_write_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+file = open('program.txt','r')
+file.read(2)
+file.readline(1)
+file.open('program.txt','w')
+file.write('Tu dep trai hon Dung')
+
+
+# In[ ]:
+
+
+
+
